a89577506061f922000252bd97066df9ef1c968a
Dennis Lin 林昱帆 <b000732001@zoho.com>
2021-07-25 16:21:12 +0800

    spring boot
    For exam project

---
+++ Cathay/src/test/java/com/CathayApplicationTests.java
@@ -0,0 +1,12 @@
+package com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class CathayApplicationTests
+{
+	@Test
+	void contextLoads() {
+	}
+}
