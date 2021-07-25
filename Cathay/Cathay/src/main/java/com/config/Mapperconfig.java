8e632b4678244b3bcd6d5d647b15ba9a70fed76b
Dennis Lin 林昱帆 <b000732001@zoho.com>
2021-07-25 16:43:51 +0800

    spring boot
    For exam

---
+++ Cathay/Cathay/src/main/java/com/config/Mapperconfig.java
@@ -0,0 +1,17 @@
+package com.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Mapperconfig
+{
+	   @Bean
+	   public ModelMapper modelMapper() {
+	      ModelMapper modelMapper = new ModelMapper();
+	      return modelMapper;
+
+	      
+	   }
+}
