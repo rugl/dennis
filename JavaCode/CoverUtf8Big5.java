f391bc5ce9a16094376537f7418504f81870723d
Dennis Lin 林昱帆 <b000732001@zoho.com>
2021-08-24 13:41:20 +0800

    Add files via upload

---
+++ JavaCode/CoverUtf8Big5.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class CoverUtf8Big5
+{
+	public void CoverUtf8Big5() {
+	String file_path = "C:\\Users\\1800342.SYSTEX\\Desktop\\utf8.txt";
+	try
+	{
+		String line = "";
+		File file = new File( file_path );
+		
+		BufferedReader br = new BufferedReader( new InputStreamReader( new FileInputStream( file ), "utf-8" ) );
+		System.out.println( file );
+		File wfile = new File( "C:\\Users\\1800342.SYSTEX\\Desktop\\big5.txt" );
+		System.out.println( wfile );
+		BufferedWriter fw = new BufferedWriter( new OutputStreamWriter( new FileOutputStream( wfile ), "big5" ) );
+		
+		while ( ( line = br.readLine( ) ) != null )
+		{
+			fw.write( line + "\n" );
+			System.out.println( line );
+		}
+		
+		// System.out.println(System.getProperty("file.encoding"));
+		fw.flush( );
+		fw.close( );
+		br.close( );
+	} catch ( Exception e )
+	{
+		System.err.println( e );
+	}
+	}
+}
